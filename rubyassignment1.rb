fb2eceb994492589354292ad9f85848c198bbb4a
Daniel Rojas <drojas@google.com>
2014-06-05 19:33:32 -0400

    first ruby commit

---
+++ rubyassignment1.rb
@@ -0,0 +1,37 @@
+#Create a function that takes a string and adds the phrase "Only in America!" to the end of it.
+# I decided to write an each loop so that it prints both items in the array with the Only in America
+# string =after it
+
+america_things = ["Cronut", "Hot dog"]
+
+america_things.each do |thing|
+	puts "#{thing}s Only in America!"
+end
+
+# having trouble finding the max value of an array. I know you mentioned using the each loop is important here
+#but not undertanding how to print only the max value
+
+#max_value_array = [1,5,10,20]
+
+#max_value_array.each do max { |a, b|  }
+
+tech_companies = {search:["google", "yahoo"], music: ["best", "sucks"]}
+
+# I know there's a way here to get the code to access the array in a way that it matches google with 
+#best and yahoo with sucks. What I don't quite understand is how keys and values are different and how
+#they play into the function here
+
+#fizzbuzz
+
+# This sounds like an if function where we print the number if not divisible by 3 or 5, print fizz if
+#divisible by 3 and buzz if divisible by 5 while fizzbuzz if divisible by both numbers 
+
+
+
+
+
+
+
+
+
+
